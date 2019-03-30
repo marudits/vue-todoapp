18c7508084809a7fe4133694d5ba9ef6792e98cf
marudits <marudi@coss.io>
2019-03-31 01:28:35 +0800

    :sparkles: NEW FEATURES | Add funcs for update, delete, and filtered get

--- server/models/posts.js
+++ server/models/posts.js
@@ -40,7 +40,23 @@ const posts = {
     getPosts: (req, res) => {
         const START_EXEC = process.hrtime();
 
-        const payload = data;
+        const { status, categories } = req.query;
+
+        let payload = data;
+
+        if(status && status.toLowerCase() === CONSTANTS.TODOS.STATUS.COMPLETED){
+            payload = payload.filter(x => x.is_done);
+        }
+
+        if(status && status.toLowerCase() === CONSTANTS.TODOS.STATUS.UNCOMPLETED){
+            payload = payload.filter(x => !x.is_done);
+        }
+
+        if(categories){
+            payload = payload.filter(item => categories.toLowerCase().split(',').indexOf(item.category) !== -1);
+        }
+
+        payload.sort((a,b) => a.due_at - b.due_at)
 
         res.status(200).json(new Envelope(true, payload, { exec_time: process.hrtime(START_EXEC) }));
     },
@@ -55,11 +71,45 @@ const posts = {
     },
     removePost: (req, res) => {
         const START_EXEC = process.hrtime();
-        res.status(200).json(new Envelope(true, { message: 'REMOVE_POSTS', params: req.params }, { exec_time: process.hrtime(START_EXEC) }));
+
+        const { id } = req.params;
+
+        const payload = data.find(x => x.id === id);
+
+        if(!payload){
+            res.status(200).json(new Envelope(false, { message: CONSTANTS.RESPONSE.CODE['404'], details: `Cant find post with id ${id}` }, { exec_time: process.hrtime(START_EXEC) }));
+            return;
+        }
+
+        data.splice(data.findIndex(x => x.id === id), 1);
+
+        res.status(200).json(new Envelope(true, payload, { exec_time: process.hrtime(START_EXEC) }));
     },
     updatePost: (req, res) => {
         const START_EXEC = process.hrtime();
-        res.status(200).json(new Envelope(true, { message: 'UPDATE_POSTS', params: req.params }, { exec_time: process.hrtime(START_EXEC) }));
+        
+        const { id } = req.params;
+        const { title, category, is_done, due_at } = req.body;
+
+        const postIndex = data.findIndex(x => x.id === id);
+
+        if(postIndex < 0){
+            res.status(200).json(new Envelope(false, { message: CONSTANTS.RESPONSE.CODE['404'], details: `Cant find post with id ${id}` }, { exec_time: process.hrtime(START_EXEC) }));
+        }
+
+        if(!req.body){
+            res.status(400).json(new Envelope(false, { message: CONSTANTS.RESPONSE.CODE['400'], details: `Value in request body is mandatory` }, { exec_time: process.hrtime(START_EXEC) }));
+            return;
+        }
+
+        data[postIndex] = Object.assign({}, data[postIndex], {
+            title: title || data[postIndex].title,
+            category: category || data[postIndex].category,
+            is_done: is_done || data[postIndex].is_done,
+            due_at: new Date(due_at).getTime() || data[postIndex].due_at,
+        });
+
+        res.status(200).json(new Envelope(true, data[postIndex], { exec_time: process.hrtime(START_EXEC) }));
     }
 }
 
